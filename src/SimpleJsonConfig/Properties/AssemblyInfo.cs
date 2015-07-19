8fd7b21bc937405c09a069fd9dacb64a15344e73
Joe <joe@kohde.io>
2015-07-19 10:29:23 +0200

    Remove schema and update nuget files

--- src/SimpleJsonConfig/Properties/AssemblyInfo.cs
+++ src/SimpleJsonConfig/Properties/AssemblyInfo.cs
@@ -1,4 +1,5 @@
-﻿using System.Reflection;
+﻿using System.Net;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
@@ -6,9 +7,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("SimpleJsonConfig")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("A minimal C# library for managing json config files in the .net framework")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Joe van der Walt")]
 [assembly: AssemblyProduct("SimpleJsonConfig")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
---
+++ src/SimpleJsonConfig/nuget-publish.bat
@@ -0,0 +1,2 @@
+nuget pack SimpleJsonConfig.csproj -IncludeReferencedProjects
+nuget push SimpleJsonConfig.1.0.0.0.nupkg
